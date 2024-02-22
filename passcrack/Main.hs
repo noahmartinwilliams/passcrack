08a08723adf9f7895af44d5cf47a4d3252e908dd
Noah Williams <noahmartinwilliams@gmail.com>
2024-02-22 13:40:31 -0600

    Add functions for converting hexadecimal strings to bytestrings.

--- passcrack/Main.hs
+++ passcrack/Main.hs
@@ -3,6 +3,11 @@ module Main (main) where
 import Crypto.Hash.SHA256
 import Data.List
 import System.IO
+import System.Environment
+import Data.Bits
+import Data.ByteString as BS
+import Prelude as P
+import Data.Word
 
 passwords :: String -> [String]
 passwords str = intern [(str !! 0)] str  where
@@ -12,29 +17,51 @@ passwords str = intern [(str !! 0)] str  where
         prev : (intern next chars)
 
     incrementChar :: String -> String -> String
+    incrementChar input chars | (P.length input) == 0 = [(chars !! 0)]
     incrementChar input chars = do
-        if (length input) == 0
+        let firstInputCharPos = getPos (input !! 0) chars 0
+            (_ : rest) = input
+        if firstInputCharPos == ((P.length chars) - 1) 
         then
-            [(chars !! 0)]
-        else do
-            let firstInputCharPos = getPos (input !! 0) chars 0
-                (_ : rest) = input
-            if firstInputCharPos == ((length chars) - 1) 
-            then
-                (chars !! 0) : (incrementChar rest chars)
-            else
-               (chars !! (firstInputCharPos + 1)) : rest
+            (chars !! 0) : (incrementChar rest chars)
+        else
+           (chars !! (firstInputCharPos + 1)) : rest
 
     getPos :: Char -> String -> Int -> Int
-    getPos str [] _ = error ("Internal error: getPos called on charset that does not contain first character of input string: \"" ++ [str] ++ "\"")
+    getPos str [] _ = error ("Internal error: getPos called on charset that does not contain first character of input string: \"" P.++ [str] P.++ "\"")
     getPos c ( h : _ ) x | c == h = x
     getPos c ( _ : r ) x = getPos c r (x + 1)
         
 
+hex2nibble :: Char -> Word8
+hex2nibble '0' = 0
+hex2nibble '1' = 1
+hex2nibble '2' = 2
+hex2nibble '3' = 3
+hex2nibble '4' = 4
+hex2nibble '5' = 5
+hex2nibble '6' = 6
+hex2nibble '7' = 7
+hex2nibble '8' = 8
+hex2nibble '9' = 9
+hex2nibble 'A' = 10
+hex2nibble 'B' = 11
+hex2nibble 'C' = 12
+hex2nibble 'D' = 13
+hex2nibble 'E' = 14
+hex2nibble 'F' = 15
+
+hex2bits :: String -> ByteString
+hex2bits [] = empty
+hex2bits ( f : s : rest ) = do
+    BS.append (BS.singleton (((hex2nibble f) `shiftL` 4) .|. (hex2nibble s) )) (hex2bits rest)
+
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
-    let pws = passwords "abcdefghijklmnopqrstuvwxyz"
-        outputs = map (\x -> x ++ "\n") pws
-        outStr = foldr (++) "" outputs
-    putStr outStr
+    args <- getArgs
+    let target = hex2bits (args P.!! 0)
+        pws = passwords "abcdefghijklmnopqrstuvwxyz"
+        outputs = P.map (\x -> x P.++ "\n") pws
+        outStr = P.foldr (P.++) "" outputs
+    System.IO.putStr outStr
